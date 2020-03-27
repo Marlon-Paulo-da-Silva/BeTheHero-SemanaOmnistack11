d9fedaf3d1fd70e1463d2a40a1b9af22ed8a2e03
Marlon Paulo <marlon.paulo.silva@outlook.com>
2020-03-27 10:08:31 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res)=>{
+    return res.json({
+        evento: 'Semana Omnistack 11 uhuull..',
+        aluno: 'Marlon'
+    });
+})
+
+app.listen(3333);
